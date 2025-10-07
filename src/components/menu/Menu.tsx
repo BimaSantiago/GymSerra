fcac97335afb9d19a7de38568f59239026fd2e50
BimaSantiago <abimaelsantiago297@gmail.com>
2025-10-06 23:49:12 -0600

    Fix sections

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,44 +1,23 @@
 import TituloPrin from "../escenciales/TituloPrin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
+import SubmenuItems from "./SubmenuItems";
 
 const Menu = () => {
   return (
     <>
       <BrowserRouter>
         <nav className="bg-green-700 text-white p-4">
-          <ul>
-            <li>
-              <Link to="/">Principal</Link>
-            </li>
-            <li>
-              <Link to="/historia">Historia</Link>
-            </li>
-            <li>
-              <Link to="/mision-vision">Mision y Vision</Link>
-            </li>
-            <li>
-              <Link to="/valores">Valores</Link>
-            </li>
-            <li>
-              <Link to="/comunidad">Comunidad</Link>
-            </li>
-            <li>
-              <Link to="/noticias">Noticias</Link>
-            </li>
-          </ul>
+          <SubmenuItems
+            data={["Inicio", "Sobre nosotros", "Comunidad", "Noticias"]}
+            paths={["/", "/nostros", "/comunidad", "/noticias"]}
+          />
         </nav>
         <TituloPrin title="ACADEMIA GYM SERRA" />
         <Routes>
           <Route path="/" element={<h1>Principal</h1>}></Route>
-          <Route path="/historia" element={<h1>Historia</h1>}></Route>
-          <Route
-            path="/mision-vision"
-            element={<h1>Mision y Vision</h1>}
-          ></Route>
-          <Route path="/valores"></Route>
-          <Route path="/comunidad"></Route>
-          <Route path="/noticias"></Route>
+          <Route path="/nosotros" element={<h1>Sobre nosotros</h1>}></Route>
+          <Route path="/comunidad" element={<h1>Comunidad</h1>}></Route>
+          <Route path="/noticias" element={<h1>Noticias</h1>}></Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/menu/SubmenuItems.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+  data: string[];
+  paths: string[];
+};
+
+const SubmenuItems = ({ data, paths }: Props) => {
+  return (
+    <>
+      <ul className="flex  list-none p-0 d-flex bg-black text-white justify-end m-0">
+        {data.map((item, index) => (
+          <li key={index} className="px-3 py-2 hover:bg-gray-700 rounded-md">
+            <Link
+              to={paths[index]}
+              className=" list-unstyled text-decoration-none text-white  px-3 py-2 rounded-md text-sm font-medium"
+            >
+              {item}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default SubmenuItems;
