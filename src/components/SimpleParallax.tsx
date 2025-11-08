9da71d82442839fccb74db1c86d7f278c0a450bf
kevinuriel519-lgtm <kevinuriel519@gmail.com>
2025-11-08 10:03:12 -0600

    responsive creo

--- src/components/SimpleParallax.tsx
+++ src/components/SimpleParallax.tsx
@@ -1,54 +1,55 @@
+// src/components/SimpleParallax.tsx
+
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 const SimpleParallax = () => {
   return (
-    <div className=" overflow-hidden w-full h-[600px] bg-orange-400">
-      <Parallax className='max-h-[600px]' pages={1.7}>
+    <div className="relative w-full h-screen overflow-hidden">
+      <Parallax pages={1.5} className="h-full">
         {/* Fondo */}
-        <ParallaxLayer offset={0} speed={0.013}>
+        <ParallaxLayer offset={0} speed={0.1}>
           <img
             src="/img/fondo.png"
             alt="Fondo"
-            className="absolute w-full h-full object-cover inset-0 overflow-visible "
+            className="absolute inset-0 w-full h-full object-cover object-center"
+          />
+        </ParallaxLayer>
+
+        {/* arbol */}
+        <ParallaxLayer offset={0} speed={0.4}>
+          <img
+            src="/img/arbol.png"
+            alt="Árbol"
+            className="absolute inset-0 w-full h-full object-cover object-center"
           />
         </ParallaxLayer>
 
         {/* Sol */}
-        <ParallaxLayer offset={0} speed={0.95}>
+        <ParallaxLayer offset={0} speed={0.7}>
           <img
             src="/img/sol.png"
             alt="Sol"
-            className="absolute inset-0 w-full h-full object-cover overflow-visible"
+            className="absolute inset-0 w-full h-full object-cover object-center"
           />
         </ParallaxLayer>
 
         {/* Atleta */}
-        <ParallaxLayer offset={0} speed={0.99}>
+        <ParallaxLayer offset={0} speed={0.9}>
           <img
             src="/img/atleta.png"
             alt="Atleta"
-            className="absolute inset-0 w-full h-full object-cover  overflow-visible"
-          />
-        </ParallaxLayer>
-
-        {/* Árbol */}
-        <ParallaxLayer className=" top-3" offset={0} speed={0.55}>
-          <img
-            src="/img/arbol.png"
-            alt="Árbol"
-            className="absolute inset-0 w-full h-full object-cover  overflow-visible"
+            className="absolute inset-0 w-full h-full object-cover object-center"
           />
         </ParallaxLayer>
 
         {/* Pasto */}
-        <ParallaxLayer offset={0} speed={0.70}>
+        <ParallaxLayer offset={0} speed={1.1}>
           <img
             src="/img/pasto.png"
             alt="Pasto"
-            className="absolute inset-0 w-full h-full object-cover overflow-visible"
+            className="absolute inset-0 w-full h-full object-cover object-center"
           />
         </ParallaxLayer>
-
       </Parallax>
     </div>
   );
